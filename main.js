8c7b0ec44243ceaa6d66c2e40516e84283d271ce
elhot <elhot999q@gmail.com>
2024-06-28 21:56:56 +0300

    add Project file

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let sliderElement = document.querySelector('.uk-slider');
+    let slider = UIkit.slider(sliderElement);
+
+    let progressBar = document.querySelector('.progress-bar');
+
+    sliderElement.addEventListener('itemshown', function() {
+        let currentIndex = slider.index; // الفهرس الحالي للشريحة
+        let totalSlides = slider.slides.length;
+        let progress = ((currentIndex + 1) / totalSlides) * 100;
+
+        progressBar.style.width = `${progress}%`;
+    });
+});
